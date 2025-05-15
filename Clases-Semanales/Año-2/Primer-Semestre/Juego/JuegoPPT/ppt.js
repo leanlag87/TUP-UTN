f4332796c6ce8ad36eeab292e79a354e87af21ca
Leandro Gonzalez <leanlag@gmail.com>
2025-05-14 21:32:07 -0300

    Fin del juego

--- Clases-Semanales/Año-2/Primer-Semestre/Juego/JuegoPPT/ppt.js
+++ Clases-Semanales/Año-2/Primer-Semestre/Juego/JuegoPPT/ppt.js
@@ -1,3 +1,59 @@
+// //1 sera piedra, 2 sera papel y 3 sera tijera
+
+// //funcion que genera un numero aleatorio entre 1 y 3
+// function randomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// function eleccion(jugada) {
+//   let resultado = "";
+//   if (jugada === 1) {
+//     resultado = "Piedra";
+//   } else if (jugada === 2) {
+//     resultado = "Papel";
+//   } else if (jugada === 3) {
+//     resultado = "Tijera";
+//   } else {
+//     resultado = "Mal elegido";
+//   }
+//   return resultado;
+// }
+
+// let jugador = 0;
+// //generamos un numero aleatorio entre 1 y 3
+// let pc = 0;
+// let triunfos = 0;
+// let derrotas = 0;
+
+// while (triunfos < 3 && derrotas < 3) {
+//   pc = randomNumber(1, 3);
+//   jugador = prompt("Elige 1 para piedra, 2 para papel y 3 para tijera");
+
+//   alert("PC elige: " + eleccion(pc));
+//   alert("Tu elige: " + eleccion(jugador));
+
+//   //alert("El jugador ha elegido: " + jugador);
+
+//   //Combate
+//   if (pc === jugador) {
+//     alert("EMpPATE");
+//   } else if (jugador == 1 && pc == 3) {
+//     alert("GANASTE");
+//     triunfos++;
+//   } else if (jugador == 2 && pc == 1) {
+//     alert("GANASTE");
+//     triunfos++;
+//   } else if (jugador == 3 && pc == 2) {
+//     alert("GANASTE");
+//     triunfos++;
+//   } else {
+//     alert("PERDISTE");
+//     derrotas++;
+//   }
+// }
+
+// alert("Ganaste " + triunfos + " veces. Perdiste " + derrotas + " veces.");
+
 //1 sera piedra, 2 sera papel y 3 sera tijera
 
 //funcion que genera un numero aleatorio entre 1 y 3
@@ -14,42 +70,66 @@ function eleccion(jugada) {
   } else if (jugada === 3) {
     resultado = "Tijera";
   } else {
-    resultado = "Mal elegido";
+    resultado = "Elegiste mal 👹";
   }
   return resultado;
 }
 
-let jugador = 0;
-//generamos un numero aleatorio entre 1 y 3
-let pc = 0;
-let triunfos = 0;
-let derrotas = 0;
+// Elementos del DOM
+const resultadoElement = document.getElementById("resultado");
+const reiniciarBtn = document.getElementById("reiniciar");
 
-while (triunfos < 3 && derrotas < 3) {
-  pc = randomNumber(1, 3);
-  jugador = prompt("Elige 1 para piedra, 2 para papel y 3 para tijera");
+// Función para iniciar el juego
+function iniciarJuego() {
+  let jugador = 0;
+  let pc = 0;
+  let triunfos = 0;
+  let derrotas = 0;
 
-  alert("PC elige: " + eleccion(pc));
-  alert("Tu elige: " + eleccion(jugador));
+  while (triunfos < 3 && derrotas < 3) {
+    pc = randomNumber(1, 3);
+    jugador = prompt("Elige 1 para piedra, 2 para papel y 3 para tijera");
 
-  //alert("El jugador ha elegido: " + jugador);
+    alert("PC elige: " + eleccion(pc));
+    alert("Tu eliges: " + eleccion(jugador));
 
-  //Combate
-  if (pc === jugador) {
-    alert("EMpPATE");
-  } else if (jugador == 1 && pc == 3) {
-    alert("GANASTE");
-    triunfos++;
-  } else if (jugador == 2 && pc == 1) {
-    alert("GANASTE");
-    triunfos++;
-  } else if (jugador == 3 && pc == 2) {
-    alert("GANASTE");
-    triunfos++;
-  } else {
-    alert("PERDISTE");
-    derrotas++;
+    //Combate
+    if (pc === jugador) {
+      alert("EMPATE");
+    } else if (jugador == 1 && pc == 3) {
+      alert("GANASTE");
+      triunfos++;
+    } else if (jugador == 2 && pc == 1) {
+      alert("GANASTE");
+      triunfos++;
+    } else if (jugador == 3 && pc == 2) {
+      alert("GANASTE");
+      triunfos++;
+    } else {
+      alert("PERDISTE");
+      derrotas++;
+    }
   }
+
+  // En lugar de mostrar un alert, actualizamos el contenido del div resultado
+  resultadoElement.innerHTML = `
+    <p>Juego finalizado</p>
+    <p>Ganaste ${triunfos} veces. Perdiste ${derrotas} veces.</p>
+  `;
+
+  // Mostramos el botón de reinicio
+  reiniciarBtn.style.display = "block";
 }
 
-alert("Ganaste " + triunfos + " veces. Perdiste " + derrotas + " veces.");
+// Evento click para el botón reiniciar
+reiniciarBtn.addEventListener("click", function () {
+  // Ocultamos el botón de reinicio
+  reiniciarBtn.style.display = "none";
+  // Limpiamos el resultado
+  resultadoElement.innerHTML = "";
+  // Reiniciamos el juego
+  iniciarJuego();
+});
+
+// Iniciamos el juego cuando carga la página
+iniciarJuego();
