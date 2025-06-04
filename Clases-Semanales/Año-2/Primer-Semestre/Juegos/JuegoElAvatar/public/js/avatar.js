9ac698f2c15879adf6d4c00675a321b5cf5e8d7c
Leandro Gonzalez <leanlag@gmail.com>
2025-06-04 19:57:13 -0300

    Agregar selección de personajes y actualización de vidas en el juego Avatar

--- Clases-Semanales/Año-2/Primer-Semestre/Juegos/JuegoElAvatar/public/js/avatar.js
+++ Clases-Semanales/Año-2/Primer-Semestre/Juegos/JuegoElAvatar/public/js/avatar.js
@@ -9,6 +9,13 @@ const btnPersonajeJugador = document.getElementById("btn-personaje");
 //Agreamos los eventos a los botones y funciones
 btnPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
 
+// Función para elegir aleatoriamente el personaje del enemigo
+function aleatoria() {
+  const personajes = ["Zuko", "Katara", "Toph", "Aang"];
+  const indiceAleatorio = Math.floor(Math.random() * personajes.length);
+  return personajes[indiceAleatorio];
+}
+
 //Creamos uuna funcion para seleccionar el personaje del jugador
 function seleccionarPersonajeJugador() {
   // Obtenemos el valor del personaje seleccionado
@@ -16,7 +23,16 @@ function seleccionarPersonajeJugador() {
     'input[name="personaje"]:checked'
   );
   if (personajeSeleccionado) {
-    alert("Seleccionaste a " + personajeSeleccionado.labels[0].innerText);
+    const personajeNombre = personajeSeleccionado.labels[0].innerText;
+
+    // Actualizamos el span con el nombre del personaje
+    document.getElementById("personaje-jugador").innerText = personajeNombre;
+
+    // Seleccionamos el personaje enemigo usando la función aleatoria
+    const personajeEnemigo = aleatoria();
+
+    // Actualizamos el span con el nombre del personaje enemigo
+    document.getElementById("personaje-enemigo").innerText = personajeEnemigo;
   } else {
     alert("No has seleccionado un personaje");
   }
