7a58c4fd0641644cb86184f2c8cedda4c28eef00
Leandro Gonzalez <leanlag@gmail.com>
2025-08-26 21:30:38 -0300

    Modificar app.js para usar 'const' en lugar de 'let' y mejorar la declaración de variables

--- Clases-Semanales/Año-2/Segundo-Semestre/node/Clase02/app.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/node/Clase02/app.js
@@ -1,5 +1,7 @@
-let nombre = "Node";
+const nombre = "Node";
 console.log(nombre);
 
-nombre = "Node.js";
-console.log(nombre);
+const nombre2 = "Node.js";
+console.log(nombre2);
+
+console.log("Hola Mundo desde Node.js");
