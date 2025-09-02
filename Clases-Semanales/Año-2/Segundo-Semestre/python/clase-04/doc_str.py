0505ba7e90090d81f409c85b238e20d30e3768f3
Leandro Gonzalez <leanlag@gmail.com>
2025-09-02 15:01:26 -0300

    Eliminar comentarios de documentación y simplificar la importación de MiClase en doc_str.py

--- Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/doc_str.py
+++ Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/doc_str.py
@@ -1,8 +1,7 @@
-"""
-Podemos crear documentación
-"""
+from mi_clase import MiClase
 
-"""
-Esta es otra manera de documentar, es decir, se puede usar comillas simples o dobles.
-Pero siempre las comillas deben ser triples.
-"""
+# De esta forma podemos ver la documentación de la clase y sus métodos
+# Podemos ver la documentacion que hicimos con docstrings en el otro archivo
+# help(MiClase)
+
+print(MiClase.__doc__)  # de esta forma vemos la doc de la clase con __doc__
