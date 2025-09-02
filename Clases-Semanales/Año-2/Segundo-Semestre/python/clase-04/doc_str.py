21b63952b551c58ead4ff72700e1eca0d4abbc95
Leandro Gonzalez <leanlag@gmail.com>
2025-09-02 15:09:23 -0300

    Eliminar comentarios de documentación innecesarios en doc_str.py

--- Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/doc_str.py
+++ Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/doc_str.py
@@ -4,4 +4,13 @@ from mi_clase import MiClase
 # Podemos ver la documentacion que hicimos con docstrings en el otro archivo
 # help(MiClase)
 
-print(MiClase.__doc__)  # de esta forma vemos la doc de la clase con __doc__
+# print(MiClase.__doc__)  # de esta forma vemos la doc de la clase con __doc__
+
+# De esta forma vemos la doc del método __init__ con __doc__
+# print(MiClase.__init__.__doc__)
+
+# de esta forma vemos la doc del método mi_metodo con __doc__
+# print(MiClase.metodo.__doc__)
+
+print(MiClase.metodo)  # de esta forma vemos la referencia al método sin ejecutarlo
+print(type(MiClase.metodo))  # de esta forma vemos que es un objeto función
