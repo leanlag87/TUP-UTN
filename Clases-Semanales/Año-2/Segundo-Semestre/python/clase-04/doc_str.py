d93ff63218b0e8330934ae873eab0e9903556e9b
Leandro Gonzalez <leanlag@gmail.com>
2025-09-02 14:52:35 -0300

    Agregar clase MiClase con documentación y método constructor en mi_clase.py

---
+++ Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/doc_str.py
@@ -0,0 +1,8 @@
+"""
+Podemos crear documentación
+"""
+
+"""
+Esta es otra manera de documentar, es decir, se puede usar comillas simples o dobles.
+Pero siempre las comillas deben ser triples.
+"""
---
+++ Clases-Semanales/Año-2/Segundo-Semestre/python/clase-04/mi_clase.py
@@ -0,0 +1,18 @@
+class MiClase:
+    """
+    Esta es la documentación de la clase MiClase.
+    """
+
+    def __init__(self):
+        """
+        Este es el método constructor de la clase MiClase.
+        """
+
+    def metodo(self, param1, param2):
+        """
+        Este es un método de la clase MiClase.
+        Detecta los parametros automáticamente.
+        :param param1: Descripción del parámetro param1
+        :param param2: Descripción del parámetro param2
+        :return: Descripción del valor de retorno
+        """
