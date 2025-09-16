d7ff78de4fb859529eb555c686cc8dce4c0b9538
Leandro Gonzalez <leanlag@gmail.com>
2025-09-16 14:16:49 -0300

    fix: Comentar código de error forzado en monohilo.js

--- Clases-Semanales/Año-2/Segundo-Semestre/node/Clase05/monohilo.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/node/Clase05/monohilo.js
@@ -8,8 +8,17 @@ setInterval(function () {
   i++;
 
   //Creamos un error forzado
-  if (i === 5) {
-    console.log("Error forzado");
-    var a = 3 + z;
-  }
+  //   if (i === 5) {
+  //     console.log("Error forzado");
+  //     var a = 3 + z;
+  //   }
 }, 1000);
+
+/**
+ * Esta es una instruccion que se ejecuta antes del setInterval
+ * luego se ejecuta el setInterval
+ * En node se ejecuta de manera monohilo, es decir cuando te toca y no de arriba a abajo
+ *
+ */
+
+console.log("Segunda instruccion");
