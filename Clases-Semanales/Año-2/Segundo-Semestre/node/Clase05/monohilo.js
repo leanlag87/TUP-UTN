bc6028d1d0f2906887250b4a3c63ca27f107a5ad
Leandro Gonzalez <leanlag@gmail.com>
2025-09-16 14:09:42 -0300

    feat: Modificar monohilo.js para mostrar el contador y forzar un error

--- Clases-Semanales/Año-2/Segundo-Semestre/node/Clase05/monohilo.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/node/Clase05/monohilo.js
@@ -1,6 +1,15 @@
 console.log("Hola Titanes del codigo!");
 
+var i = 0;
+
 //Creamos una funcion para que no se cierre la consola
 setInterval(function () {
-  console.log("Sigo ejecutandome...");
+  console.log(i);
+  i++;
+
+  //Creamos un error forzado
+  if (i === 5) {
+    console.log("Error forzado");
+    var a = 3 + z;
+  }
 }, 1000);
