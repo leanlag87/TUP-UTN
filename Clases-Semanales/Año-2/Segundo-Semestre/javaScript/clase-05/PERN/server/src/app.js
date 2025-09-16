b5fecffbc5659c40b6908ff574f57c0e2319fe75
Leandro Gonzalez <leanlag@gmail.com>
2025-09-16 14:42:19 -0300

    Clase 05 de JS cuarto semestre

---
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-05/PERN/server/src/app.js
@@ -0,0 +1,31 @@
+//Aqui va estar el codigo del servidor de express
+import express from "express";
+import morgan from "morgan";
+import tasksRoutes from "./router/tasks.routes.js";
+import authRoutes from "./router/auth.routes.js";
+
+// Exportamos e instanciamos la app de express
+const app = express();
+
+//usamos morgan para ver las peticiones por consola
+app.use(morgan("dev"));
+
+// Para que el servidor entienda JSON
+app.use(express.json());
+
+// Para que el servidor entienda datos de formularios
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ message: "Bienvenidos a nuestro proyecto" })
+);
+app.use("/api", tasksRoutes);
+app.use("/api", authRoutes);
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  res.status(500).json({ status: "error", message: err.message });
+});
+
+// Exportamos la app para usarla en otros archivos
+export default app;
---
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-05/PERN/server/src/router/auth.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  getProfile,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller";
+
+const router = Router();
+
+// Aqui van a estar las rutas relacionadas con la autenticación
+
+router.post("/login", login);
+
+router.post("/register", register);
+
+router.post("/logout", logout);
+
+router.get("/profile", getProfile);
+
+export default router;
