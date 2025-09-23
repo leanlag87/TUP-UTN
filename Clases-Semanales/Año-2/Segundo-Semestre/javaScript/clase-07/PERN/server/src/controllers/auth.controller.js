84081e9b44c564c07e037594ec0f0ab98b59749b
Leandro Gonzalez <leanlag@gmail.com>
2025-09-23 15:06:25 -0300

    feat: Agregar tablas de usuarios y tareas en la base de datos

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-07/PERN/server/src/controllers/auth.controller.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-07/PERN/server/src/controllers/auth.controller.js
@@ -1,10 +1,20 @@
+import { pool } from "../db.js";
+
 // Controlador para la autenticación
 
 export const login = (req, res) =>
   res.send("Iniciando sesión con datos: " + JSON.stringify(req.body));
 
-export const register = (req, res) =>
-  res.send("Registrando usuario con exito: " + JSON.stringify(req.body));
+export const register = (req, res) => {
+  const { name, email, password } = req.body;
+  res.send(`Registrando usuario: ${name}, Email: ${email}`);
+
+  pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3)", [
+    name,
+    email,
+    password,
+  ]);
+};
 
 export const logout = (req, res) => res.send("Cerrando sesión");
 
