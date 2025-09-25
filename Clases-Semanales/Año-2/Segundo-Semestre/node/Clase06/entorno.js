87d9f2c5f74f5ffb62840593fadc82789da35023
Leandro Gonzalez <leanlag@gmail.com>
2025-09-25 16:26:20 -0300

    feat: Agregar variable web y mejorar la salida de consola en entorno.js

--- Clases-Semanales/Año-2/Segundo-Semestre/node/Clase06/entorno.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/node/Clase06/entorno.js
@@ -1,3 +1,7 @@
 let nombre = process.env.NOMBRE || "Sin Nombre";
+let web = process.env.WEB || "No tengo web";
 
-console.log(`Hola ${nombre}`);
+console.log("Nombre:", nombre);
+console.log("Web:", web);
+
+//console.log(`Hola ${nombre}`);
