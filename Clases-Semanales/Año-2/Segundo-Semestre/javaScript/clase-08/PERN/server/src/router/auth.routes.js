5122b71739a4f348ccd491c6352e02ff446bd0e2
Leandro Gonzalez <leanlag@gmail.com>
2025-10-21 11:20:57 -0300

    feat: Initialize server with Express and PostgreSQL setup
    - Created package.json with necessary dependencies and scripts.
    - Implemented main Express application in app.js with middleware for logging and JSON parsing.
    - Added configuration file for server settings.
    - Developed authentication controller with login and registration functionalities, including password hashing and JWT token generation.
    - Implemented tasks controller for CRUD operations on tasks.
    - Established database connection using pg and dotenv for environment variables.
    - Set up routing for authentication and task management.
    - Created entry point for the server in index.js.

---
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-08/PERN/server/src/router/auth.routes.js
@@ -0,0 +1,22 @@
+//import { Router } from "express";
+import Router from "express-promise-router";
+import {
+  getProfile,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller.js";
+
+const router = Router();
+
+// Aqui van a estar las rutas relacionadas con la autenticación
+
+router.post("/login", login);
+
+router.post("/register", register);
+
+router.post("/logout", logout);
+
+router.get("/profile", getProfile);
+
+export default router;
---
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-08/PERN/server/src/router/tasks.routes.js
@@ -0,0 +1,24 @@
+import Router from "express-promise-router";
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateTask,
+} from "../controllers/tasks.controller.js";
+
+const router = Router();
+
+// Aqui van a estar las rutas relacionadas con las tareas
+
+router.get("/tasks", getTasks);
+
+router.get("/tasks/:id", getTaskById);
+
+router.post("/tasks", createTask);
+
+router.put("/tasks/:id", updateTask);
+
+router.delete("/tasks/:id", deleteTask);
+
+export default router;
