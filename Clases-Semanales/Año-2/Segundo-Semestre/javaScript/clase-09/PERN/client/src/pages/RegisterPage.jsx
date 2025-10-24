42b34bed932542407e80e2c643df7319bc1d8656
Leandro Gonzalez <leanlag@gmail.com>
2025-10-24 15:41:56 -0300

    feat: implementar página de registro con formulario y componente de entrada

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/pages/RegisterPage.jsx
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/pages/RegisterPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Input from "../components/ui/input";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">RegisterPage</h3>
+
+      <form>
+        <Input placeholder="Ingrese Su Nombre" />
+        <Input type="email" placeholder="Ingrese Su Correo " />
+        <Input type="password" placeholder="Ingrese Su Contraseña" />
+
+        <button>Registrarse</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
