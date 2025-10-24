339c178ee766a467953d8dda6c24244164808aae
Leandro Gonzalez <leanlag@gmail.com>
2025-10-24 16:54:38 -0300

    feat: agregar soporte para react-hook-form en la página de registro

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/pages/RegisterPage.jsx
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/pages/RegisterPage.jsx
@@ -1,14 +1,48 @@
+import { useForm } from "react-hook-form";
 import { Button, Card, Input } from "../components/ui";
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <Card>
         <h3 className="text-2xl font-bold">Registro</h3>
-        <form>
-          <Input placeholder="Ingrese Su Nombre" />
-          <Input type="email" placeholder="Ingrese Su Correo " />
-          <Input type="password" placeholder="Ingrese Su Contraseña" />
+        <form
+          className="flex flex-col gap-4 mt-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Input
+            placeholder="Ingrese Su Nombre"
+            {...register("name", { required: true })}
+          />
+          {errors.name && (
+            <span className="text-red-500">Este campo es requerido</span>
+          )}
+          <Input
+            type="email"
+            placeholder="Ingrese Su Correo "
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span className="text-red-500">Este campo es requerido</span>
+          )}
+          <Input
+            type="password"
+            placeholder="Ingrese Su Contraseña"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <span className="text-red-500">Este campo es requerido</span>
+          )}
 
           <Button>Registrarse</Button>
         </form>
