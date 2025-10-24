9f6e8f8cdff6549330f1e3eb3d83f6feac2baed7
Leandro Gonzalez <leanlag@gmail.com>
2025-10-24 15:01:27 -0300

    feat: agregar rutas y páginas principales a la aplicación

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/App.jsx
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-09/PERN/client/src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import TasksPage from "./pages/TasksPage.jsx";
+import TaskFormPage from "./pages/TaskFormPage.jsx";
+
 const App = () => {
   return (
-    <div>
-      <h1>hola mundo</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/tasks" element={<TasksPage />} />
+      <Route path="/tasks/new" element={<TaskFormPage />} />
+      <Route path="/tasks/edit/:id" element={<TaskFormPage />} />
+    </Routes>
   );
 };
 
