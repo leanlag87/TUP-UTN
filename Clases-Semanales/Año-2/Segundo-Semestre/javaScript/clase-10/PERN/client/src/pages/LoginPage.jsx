ee0d44088114f5e1055f31673aaf127a6b6c5249
Leandro Gonzalez <leanlag@gmail.com>
2025-10-25 12:07:51 -0300

    feat: implementar la página de inicio de sesión con formulario y estilos

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-10/PERN/client/src/pages/LoginPage.jsx
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-10/PERN/client/src/pages/LoginPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Button, Card, Input } from "../components/ui";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h1 className="text-4xl font-bold my-2 text-center">Iniciar Sesión</h1>
+
+        <form className="flex flex-col gap-4 w-80">
+          <Input type="email" placeholder="Ingrese su correo" required />
+          <Input type="password" placeholder="Ingrese su contraseña" required />
+          <Button type="submit" className="mt-4">
+            Ingresar
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default LoginPage;
