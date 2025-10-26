65bc0c7798c3c08bfbb59f59c8f7648cded4e9d3
Leandro Gonzalez <leanlag@gmail.com>
2025-10-26 17:02:08 -0300

    feat: implementar rutas protegidas y mejorar la navegación según estado de autenticación

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-10/PERN/client/src/App.jsx
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-10/PERN/client/src/App.jsx
@@ -9,21 +9,41 @@ import TaskFormPage from "./pages/TaskFormPage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import { Container } from "./components/ui/Container.jsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
+import { useAuth } from "./context/AuthContext.jsx";
 
 const App = () => {
+  const { isAuth } = useAuth();
+
   return (
     <>
       <Navbar />
       <Container className="py-5">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<TasksPage />} />
-          <Route path="/tasks/new" element={<TaskFormPage />} />
-          <Route path="/tasks/edit/:id" element={<TaskFormPage />} />
+          {/* Rutas públicas accesibles solo si no está autenticado "!isAuth"
+           Si el usuario está autenticado, será redirigido a la página de tareas
+            Usamos la propiedad redirectTo para definir la ruta de redirección*/}
+          <Route
+            element={<ProtectedRoute isAllowed={!isAuth} redirectTo="/tasks" />}
+          >
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          {/* Rutas protegidas accesibles solo si está autenticado "isAuth"
+           Si el usuario no está autenticado, será redirigido a la página de inicio de sesión
+           Usamos la propiedad redirectTo para definir la ruta de redirección */}
+          <Route
+            element={<ProtectedRoute isAllowed={isAuth} redirectTo="/login" />}
+          >
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/tasks/new" element={<TaskFormPage />} />
+            <Route path="/tasks/edit/:id" element={<TaskFormPage />} />
+          </Route>
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
