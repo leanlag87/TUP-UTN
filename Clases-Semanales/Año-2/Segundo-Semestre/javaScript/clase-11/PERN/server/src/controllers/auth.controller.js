1aeb222528650b4e4f710b4382845669bb0296ff
Leandro Gonzalez <leanlag@gmail.com>
2025-10-29 23:57:50 -0300

    feat: mejorar controlador de autenticación para incluir manejo de errores y devolver información del usuario en el login y registro

--- Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-11/PERN/server/src/controllers/auth.controller.js
+++ Clases-Semanales/Año-2/Segundo-Semestre/javaScript/clase-11/PERN/server/src/controllers/auth.controller.js
@@ -4,48 +4,52 @@ import createAccessToken from "../libs/jwt.js";
 import md5 from "md5";
 
 // Controlador para la autenticación
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   const { email, password } = req.body; // Obtener email y password del cuerpo de la solicitud
 
-  //Confirmamos si el usuario existe
-  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
-    email,
-  ]);
+  try {
+    //Confirmamos si el usuario existe
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
-  //Si no existe, retornamos un error
-  if (result.rows.length === 0) {
-    return res.status(400).json({ error: "Usuario no encontrado" });
+    //Si no existe, retornamos un error
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "Usuario no encontrado" });
+    }
+
+    //Si existe, comparamos la contraseña
+    const user = result.rows[0];
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    //Si la contraseña no es válida, retornamos un error
+    if (!validPassword) {
+      return res.status(400).json({ error: "Contraseña incorrecta" });
+    }
+
+    //Si la contraseña es válida, creamos un token de acceso
+    // Crear un token de acceso
+    const token = await createAccessToken({ id: result.rows[0].id });
+    console.log(result);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    return res.json({
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        gravatar: user.gravatar_url,
+      },
+    });
+  } catch (error) {
+    console.error("Error durante el login:", error);
+    next(error);
   }
-
-  //Si existe, comparamos la contraseña
-  const user = result.rows[0];
-  const validPassword = await bcrypt.compare(password, user.password);
-
-  //Si la contraseña no es válida, retornamos un error
-  if (!validPassword) {
-    return res.status(400).json({ error: "Contraseña incorrecta" });
-  }
-
-  //Si la contraseña es válida, creamos un token de acceso
-  // Crear un token de acceso
-  const token = await createAccessToken({ id: result.rows[0].id });
-  console.log(result);
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  });
-
-  return res.json(result.rows[0]);
-  // return res.json({
-  //   token: token,
-  //   user: {
-  //     id: user.id,
-  //     name: user.name,
-  //     email: user.email,
-  //   },
-  // });
 };
 
 export const register = async (req, res, next) => {
@@ -77,8 +81,14 @@ export const register = async (req, res, next) => {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
-    //return res.json(result.rows[0]);
-    return res.json({ token: token });
+    return res.json({
+      user: {
+        id: result.rows[0].id,
+        name: result.rows[0].name,
+        email: result.rows[0].email,
+        gravatar: result.rows[0].gravatar_url,
+      },
+    });
   } catch (error) {
     if (error.code === "23505") {
       // Código de error para violación de restricción única
@@ -87,12 +97,9 @@ export const register = async (req, res, next) => {
         .json({ error: "Error: El correo electrónico ya está en uso." });
     }
 
+    console.error("Error durante el registro:", error);
     next(error);
   }
-  // Manejar otros errores
-  //   console.error("Error durante el registro:", error);
-  //   return res.status(500).json({ error: "Error interno del servidor" });
-  // }
 };
 
 export const logout = (req, res) => {
